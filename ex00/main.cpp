e843037f9d83e8624dfcf64546dc36215f51045b
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-09-16 05:24:35 +0900

    :dizzy:[update]main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-//#include "Animal.hpp"
-//#include "Cat.hpp"
-//#include "Dog.hpp"
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+//#include "WrongAnimal.hpp"
+//#include "WrongCat.hpp"
 
 //__attribute__((destructor))
 //static void destructor() {
@@ -12,27 +12,27 @@
 
 int main()
 {
-//	const Animal *meta = new Animal();
-//	const Animal *j = new Dog();
-//	const Animal *i = new Cat();
-//	std::cout << j->getType() << " " << std::endl;
-//	std::cout << i->getType() << " " << std::endl;
-//	i->makeSound();
-//	j->makeSound();
-//	meta->makeSound();
+	const Animal *meta = new Animal();
+	const Animal *j = new Dog();
+	const Animal *i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+
+	delete meta;
+	delete j;
+	delete i;
+
+//	const WrongAnimal *mw = new WrongAnimal();
+//	const WrongAnimal *iw = new WrongCat();
+//	std::cout << mw->getType() << " " << std::endl;
+//	std::cout << iw->getType() << " " << std::endl;
+//	iw->makeSound();
+//	mw->makeSound();
 //
-//	delete meta;
-//	delete j;
-//	delete i;
-
-	const WrongAnimal *mw = new WrongAnimal();
-	const WrongAnimal *iw = new WrongCat();
-	std::cout << mw->getType() << " " << std::endl;
-	std::cout << iw->getType() << " " << std::endl;
-	iw->makeSound();
-	mw->makeSound();
-
-	delete mw;
-	delete iw;
+//	delete mw;
+//	delete iw;
 	return 0;
 }
