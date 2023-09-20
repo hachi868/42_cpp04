5cd1b1353266f060d80dc2e2e9d406cc0648d0af
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-09-20 14:35:11 +0900

    :dizzy:[update]ex00 test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -13,7 +13,7 @@ static void destructor() {
 int main()
 {
 	{
-		std::cout << "[test1] Animal" << std::endl;
+		std::cout << "[test0] Animal" << std::endl;
 		const Animal *meta = new Animal();
 		const Animal *j = new Dog();
 		const Animal *i = new Cat();
@@ -31,7 +31,7 @@ int main()
 	{
 		//	Animal =>  WrongAnimal
 		//	Cat => WrongCat
-		std::cout << "[test2] WrongAnimal" << std::endl;
+		std::cout << "[test1] WrongAnimal" << std::endl;
 		const WrongAnimal *mw = new WrongAnimal();
 		const WrongAnimal *iw = new WrongCat();
 		iw->makeSound();
@@ -39,43 +39,45 @@ int main()
 		delete mw;
 		delete iw;
 	}
-//	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
-//	{
-//		std::cout << "[test3] Default constructor" << std::endl;
-//		Cat cat0;
-//		Dog dog0;
-//		std::cout << cat0.getType() << std::endl;
-//		std::cout << dog0.getType() << std::endl;
-//		cat0.makeSound();
-//		dog0.makeSound();
-//
-//		std::cout << "[test5] Copy constructor" << std::endl;
-//		Cat cat1(cat0);
-//		Dog dog1(dog0);
-//		std::cout << cat1.getType() << std::endl;
-//		std::cout << dog1.getType() << std::endl;
-//		cat1.makeSound();
-//		dog1.makeSound();
-//
-//		std::cout << "[test6] Copy constructor" << std::endl;
-//		Cat &cat3a = cat0;
-//		Cat cat3 = cat3a;
-//		Dog &dog3a = dog0;
-//		Dog dog3 = dog3a;
-//		std::cout << cat3.getType() << std::endl;
-//		std::cout << dog3.getType() << std::endl;
-//		cat3.makeSound();
-//		dog3.makeSound();
-//	}
-//	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
-//	{
-//		std::cout << "[test7] Copy assignment & name constructor" << std::endl;
-//		Cat cat4 = Cat();
-//		Dog dog4 = Dog();
-//		std::cout << cat4.getType() << std::endl;
-//		std::cout << dog4.getType() << std::endl;
-//		cat4.makeSound();
-//		dog4.makeSound();
-//	}
+	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
+	{
+		std::cout << "[test2] Default constructor" << std::endl;
+		Cat cat0;
+		Dog dog0;
+		std::cout << cat0.getType() << std::endl;
+		std::cout << dog0.getType() << std::endl;
+		cat0.makeSound();
+		dog0.makeSound();
+		std::cout << "----------" << std::endl;
+
+		std::cout << "[test3] Copy constructor" << std::endl;
+		Cat cat1(cat0);
+		Dog dog1(dog0);
+		std::cout << cat1.getType() << std::endl;
+		std::cout << dog1.getType() << std::endl;
+		cat1.makeSound();
+		dog1.makeSound();
+		std::cout << "----------" << std::endl;
+
+		std::cout << "[test4] Copy constructor" << std::endl;
+		Cat &cat3a = cat0;
+		Cat cat3 = cat3a;
+		Dog &dog3a = dog0;
+		Dog dog3 = dog3a;
+		std::cout << cat3.getType() << std::endl;
+		std::cout << dog3.getType() << std::endl;
+		cat3.makeSound();
+		dog3.makeSound();
+	}
+	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
+	{
+		std::cout << "[test5] Copy assignment & name constructor" << std::endl;
+		Cat cat4 = Cat();
+		Dog dog4 = Dog();
+		std::cout << cat4.getType() << std::endl;
+		std::cout << dog4.getType() << std::endl;
+		cat4.makeSound();
+		dog4.makeSound();
+	}
 	return (0);
 }
