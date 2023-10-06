44f6fe8f0b0c63843053e755c557a36c06651b86
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-10-06 13:42:52 +0900

    :dizzy:[update]ex01,02 tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -99,5 +99,50 @@ int main()
 //		cat4.showBrainInfo();
 //		dog4.showBrainInfo();
 //	}
+//	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
+//	{
+//		std::cout << "[test6] deep copy" << std::endl;
+//
+//		Cat a;
+//		std::cout << "/// 001 /// a.printIdeas" << std::endl << std::endl;
+//		a.printIdeas();
+//		a.updateIdea(1, "Idea_a1");
+//		a.updateIdea(3, "Idea_a3");
+//		a.updateIdea(5, "Idea_a5");
+//		a.updateIdea(7, "Idea_a7");
+//		a.updateIdea(9, "Idea_a9");
+//		std::cout << "/// 002 /// a.printIdeas(upd)" << std::endl << std::endl;
+//		a.printIdeas();
+//		std::cout << "Copy brain from a to b" << std::endl;
+//		{
+//			Cat b(a);
+//			std::cout << "/// 003 /// b.printIdeas(a copy)" << std::endl << std::endl;
+//			b.printIdeas();
+//			b.updateIdea(2, "Idea_b2");
+//			b.updateIdea(4, "Idea_b4");
+//			b.updateIdea(6, "Idea_b6");
+//			b.updateIdea(8, "Idea_b8");
+//			b.updateIdea(10, "Idea_b10");
+//			std::cout << "/// 004 /// b.printIdeas(a copy upd)" << std::endl << std::endl;
+//			b.printIdeas();
+//		}
+//		std::cout << "After b is destroyed" << std::endl;
+//		std::cout << "/// 005 /// a.printIdeas after b destroyed" << std::endl << std::endl;
+//		a.printIdeas();
+//		std::cout << "Copy brain from a to b" << std::endl;
+//		{
+//			Cat c = a;
+//			std::cout << "/// 006 /// c.printIdeas(a copy)" << std::endl << std::endl;
+//			c.printIdeas();
+//			c.updateIdea(1, "Idea_c1");
+//			c.updateIdea(2, "Idea_c2");
+//			c.updateIdea(3, "Idea_c3");
+//			c.updateIdea(5, "Idea_c5");
+//			c.updateIdea(7, "Idea_c7");
+//			c.updateIdea(9, "Idea_c9");
+//			std::cout << "/// 007 /// c.printIdeas(a copy upd)" << std::endl << std::endl;
+//			c.printIdeas();
+//		}
+//	}
 	return (0);
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -12,6 +12,9 @@ __attribute__((destructor)) static void destructor()
 
 int main()
 {
+//	std::cout << "*-*-*-*-*-*-*" << std::endl;
+//	std::cout << "*-* ex00 *-*" << std::endl;
+//	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
 //	{
 //		std::cout << "[test0] Animal" << std::endl;
 //		//const Animal *meta = new Animal(); error!!
@@ -122,41 +125,41 @@ int main()
 //			i++;
 //		}
 //	}
-//	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
-//	{
-//		std::cout << "[test2] Default constructor" << std::endl;
-//		Cat cat0;
-//		Dog dog0;
-//		std::cout << cat0.getType() << std::endl;
-//		std::cout << dog0.getType() << std::endl;
-//		cat0.makeSound();
-//		dog0.makeSound();
-//		cat0.showBrainInfo();
-//		dog0.showBrainInfo();
-//		std::cout << "----------" << std::endl << std::endl;
-//		std::cout << "[test3] Copy constructor" << std::endl;
-//		Cat cat1(cat0);
-//		Dog dog1(dog0);
-//		std::cout << cat1.getType() << std::endl;
-//		std::cout << dog1.getType() << std::endl;
-//		cat1.makeSound();
-//		dog1.makeSound();
-//		cat1.showBrainInfo();
-//		dog1.showBrainInfo();
-//		std::cout << "----------" << std::endl << std::endl;
-//
-//		std::cout << "[test4] Copy constructor" << std::endl;
-//		Cat &cat3a = cat0;
-//		Cat cat3 = cat3a;
-//		Dog &dog3a = dog0;
-//		Dog dog3 = dog3a;
-//		std::cout << cat3.getType() << std::endl;
-//		std::cout << dog3.getType() << std::endl;
-//		cat3.makeSound();
-//		dog3.makeSound();
-//		cat3.showBrainInfo();
-//		dog3.showBrainInfo();
-//	}
+	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
+	{
+		std::cout << "[test2] Default constructor" << std::endl;
+		Cat cat0;
+		Dog dog0;
+		std::cout << cat0.getType() << std::endl;
+		std::cout << dog0.getType() << std::endl;
+		cat0.makeSound();
+		dog0.makeSound();
+		cat0.showBrainInfo();
+		dog0.showBrainInfo();
+		std::cout << "----------" << std::endl << std::endl;
+		std::cout << "[test3] Copy constructor" << std::endl;
+		Cat cat1(cat0);
+		Dog dog1(dog0);
+		std::cout << cat1.getType() << std::endl;
+		std::cout << dog1.getType() << std::endl;
+		cat1.makeSound();
+		dog1.makeSound();
+		cat1.showBrainInfo();
+		dog1.showBrainInfo();
+		std::cout << "----------" << std::endl << std::endl;
+
+		std::cout << "[test4] Copy constructor" << std::endl;
+		Cat &cat3a = cat0;
+		Cat cat3 = cat3a;
+		Dog &dog3a = dog0;
+		Dog dog3 = dog3a;
+		std::cout << cat3.getType() << std::endl;
+		std::cout << dog3.getType() << std::endl;
+		cat3.makeSound();
+		dog3.makeSound();
+		cat3.showBrainInfo();
+		dog3.showBrainInfo();
+	}
 //	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
 //	{
 //		std::cout << "[test5] Copy assignment & name constructor" << std::endl;
@@ -169,50 +172,50 @@ int main()
 //		cat4.showBrainInfo();
 //		dog4.showBrainInfo();
 //	}
-	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
-	{
-		std::cout << "[test6] deep copy" << std::endl;
-
-		Cat a;
-		std::cout << "/// 001 /// a.printIdeas" << std::endl << std::endl;
-		a.printIdeas();
-		a.updateIdea(1, "Idea_a1");
-		a.updateIdea(3, "Idea_a3");
-		a.updateIdea(5, "Idea_a5");
-		a.updateIdea(7, "Idea_a7");
-		a.updateIdea(9, "Idea_a9");
-		std::cout << "/// 002 /// a.printIdeas(upd)" << std::endl << std::endl;
-		a.printIdeas();
-		std::cout << "Copy brain from a to b" << std::endl;
-		{
-			Cat b(a);
-			std::cout << "/// 003 /// b.printIdeas(a copy)" << std::endl << std::endl;
-			b.printIdeas();
-			b.updateIdea(2, "Idea_b2");
-			b.updateIdea(4, "Idea_b4");
-			b.updateIdea(6, "Idea_b6");
-			b.updateIdea(8, "Idea_b8");
-			b.updateIdea(10, "Idea_b10");
-			std::cout << "/// 004 /// b.printIdeas(a copy upd)" << std::endl << std::endl;
-			b.printIdeas();
-		}
-		std::cout << "After b is destroyed" << std::endl;
-		std::cout << "/// 005 /// a.printIdeas after b destroyed" << std::endl << std::endl;
-		a.printIdeas();
-		std::cout << "Copy brain from a to b" << std::endl;
-		{
-			Cat c = a;
-			std::cout << "/// 006 /// c.printIdeas(a copy)" << std::endl << std::endl;
-			c.printIdeas();
-			c.updateIdea(1, "Idea_c1");
-			c.updateIdea(2, "Idea_c2");
-			c.updateIdea(3, "Idea_c3");
-			c.updateIdea(5, "Idea_c5");
-			c.updateIdea(7, "Idea_c7");
-			c.updateIdea(9, "Idea_c9");
-			std::cout << "/// 007 /// c.printIdeas(a copy upd)" << std::endl << std::endl;
-			c.printIdeas();
-		}
-	}
+//	std::cout << "*-*-*-*-*-*-*" << std::endl << std::endl;
+//	{
+//		std::cout << "[test6] deep copy" << std::endl;
+//
+//		Cat a;
+//		std::cout << "/// 001 /// a.printIdeas" << std::endl << std::endl;
+//		a.printIdeas();
+//		a.updateIdea(1, "Idea_a1");
+//		a.updateIdea(3, "Idea_a3");
+//		a.updateIdea(5, "Idea_a5");
+//		a.updateIdea(7, "Idea_a7");
+//		a.updateIdea(9, "Idea_a9");
+//		std::cout << "/// 002 /// a.printIdeas(upd)" << std::endl << std::endl;
+//		a.printIdeas();
+//		std::cout << "Copy brain from a to b" << std::endl;
+//		{
+//			Cat b(a);
+//			std::cout << "/// 003 /// b.printIdeas(a copy)" << std::endl << std::endl;
+//			b.printIdeas();
+//			b.updateIdea(2, "Idea_b2");
+//			b.updateIdea(4, "Idea_b4");
+//			b.updateIdea(6, "Idea_b6");
+//			b.updateIdea(8, "Idea_b8");
+//			b.updateIdea(10, "Idea_b10");
+//			std::cout << "/// 004 /// b.printIdeas(a copy upd)" << std::endl << std::endl;
+//			b.printIdeas();
+//		}
+//		std::cout << "After b is destroyed" << std::endl;
+//		std::cout << "/// 005 /// a.printIdeas after b destroyed" << std::endl << std::endl;
+//		a.printIdeas();
+//		std::cout << "Copy brain from a to b" << std::endl;
+//		{
+//			Cat c = a;
+//			std::cout << "/// 006 /// c.printIdeas(a copy)" << std::endl << std::endl;
+//			c.printIdeas();
+//			c.updateIdea(1, "Idea_c1");
+//			c.updateIdea(2, "Idea_c2");
+//			c.updateIdea(3, "Idea_c3");
+//			c.updateIdea(5, "Idea_c5");
+//			c.updateIdea(7, "Idea_c7");
+//			c.updateIdea(9, "Idea_c9");
+//			std::cout << "/// 007 /// c.printIdeas(a copy upd)" << std::endl << std::endl;
+//			c.printIdeas();
+//		}
+//	}
 	return (0);
 }
